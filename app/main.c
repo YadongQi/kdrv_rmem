80f3deb67e03066eb69f61e100a607cc9e05c906
Yadong Qi <yadong.qi@intel.com>
2023-05-12 13:01:40 +0800

    Support dump continous memory
    Signed-off-by: Yadong Qi <yadong.qi@intel.com>

--- app/main.c
+++ app/main.c
@@ -8,23 +8,30 @@
 #include "../driver/kdrv_rmem.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc != 3) {
         printf("Usage:\n");
-        printf("    %s [physical addr]\n", argv[0]);
+        printf("    %s [physical addr] [offset count]\n", argv[0]);
         return -1;
     }
 
     struct msg m = { 0 };
     m.addr = strtoul(argv[1], NULL, 0);
-    printf("phyaddr=0x%lx\n", m.addr);
+    unsigned int count = strtoul(argv[2], NULL, 0);
+    if (count > 512) {
+        printf("offset count too large!\n");
+        return -1;
+    }
 
     int fd = open("/dev/rmem", O_RDWR);
     if (fd < 0) {
         printf("Failed to open /dev/rmem\n");
         return -1;
     }
-    ioctl(fd, RMEM_READ, &m);
-    printf("data: 0x%lx\n", m.data);
+    for (int i = 0; i < count; i++) {
+        ioctl(fd, RMEM_READ, &m);
+        printf("0x%lx: 0x%lx\n", m.addr, m.data);
+        m.addr += 0x8;
+    }
 
     close(fd);
 
